49753a843397cf4840135dee95afaaa930aee355
xiaopihaier <1428518169@qq.com>
2016-12-28 15:10:02 +0800

    随心听

--- app/src/main/java/xiaopihaier/com/login/Main.java
+++ app/src/main/java/xiaopihaier/com/login/Main.java
@@ -1,20 +1,47 @@
 package xiaopihaier.com.login;
 
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class Main extends AppCompatActivity implements View.OnClickListener{
 
     LinearLayout one,two,three,four;
+    ViewPager viewPager;
+    PagerAdapter pagerAdapter=new PagerAdapter() {
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return false;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            super.destroyItem(container, position, object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            return super.instantiateItem(container, position);
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         IntentView();
+        viewPager.setAdapter(pagerAdapter);
     }
 
     private void IntentView() {
@@ -26,6 +53,7 @@ public class Main extends AppCompatActivity implements View.OnClickListener{
         three.setOnClickListener(this);
         four= (LinearLayout) findViewById(R.id.four);
         four.setOnClickListener(this);
+        viewPager= (ViewPager) findViewById(R.id.vp_tabs);
     }
 
     @Override
