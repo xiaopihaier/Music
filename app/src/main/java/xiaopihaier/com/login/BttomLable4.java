a28b4756c0992d5d4973fb52d44e06cdcd536ab6
xiaopihaier <1428518169@qq.com>
2016-12-28 15:00:36 +0800

    随心听

---
+++ app/src/main/java/xiaopihaier/com/login/BttomLable4.java
@@ -0,0 +1,16 @@
+package xiaopihaier.com.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 背书包的小屁孩儿 on 16-12-28.
+ */
+
+public class BttomLable4 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bttom_lable4);
+    }
+}
