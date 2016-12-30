01918152bf71c4de06e7f62071f5daef88fdca58
xiaopihaier <1428518169@qq.com>
2016-12-30 09:51:18 +0800

    随心听

--- app/src/main/java/xiaopihaier/com/login/Main.java
+++ app/src/main/java/xiaopihaier/com/login/Main.java
@@ -11,15 +11,14 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class Main extends AppCompatActivity implements View.OnClickListener{
+public class Main extends AppCompatActivity {
 
-    LinearLayout one,two,three,four;
     ViewPager viewPager;
-    View view_1,view_2,view_3,view_4;
+    View view_1, view_2, view_3, view_4;
     ArrayList<View> arrayList;
 
     //创建适配器
-    PagerAdapter pagerAdapter=new PagerAdapter() {
+    PagerAdapter pagerAdapter = new PagerAdapter() {
         //获取viewpager的页数
         @Override
         public int getCount() {
@@ -28,13 +27,15 @@ public class Main extends AppCompatActivity implements View.OnClickListener{
 
         @Override
         public boolean isViewFromObject(View view, Object object) {
-            return view==object;
+            return view == object;
         }
+
         //销毁
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
             container.removeView(arrayList.get(position));
         }
+
         //创建
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
@@ -50,12 +51,12 @@ public class Main extends AppCompatActivity implements View.OnClickListener{
         setContentView(R.layout.activity_main);
         IntentView();
         //将布局文件加载成view视图
-        view_1=getLayoutInflater().inflate(R.layout.bttom_table1,null);
-        view_2=getLayoutInflater().inflate(R.layout.bttom_lable2,null);
-        view_3=getLayoutInflater().inflate(R.layout.bttom_lable3,null);
-        view_4=getLayoutInflater().inflate(R.layout.bttom_lable4,null);
+        view_1 = getLayoutInflater().inflate(R.layout.bttom_table1, null);
+        view_2 = getLayoutInflater().inflate(R.layout.bttom_lable2, null);
+        view_3 = getLayoutInflater().inflate(R.layout.bttom_lable3, null);
+        view_4 = getLayoutInflater().inflate(R.layout.bttom_lable4, null);
 
-        arrayList=new ArrayList();
+        arrayList = new ArrayList();
         arrayList.add(view_1);
         arrayList.add(view_2);
         arrayList.add(view_3);
@@ -65,32 +66,7 @@ public class Main extends AppCompatActivity implements View.OnClickListener{
     }
 
     private void IntentView() {
-        one= (LinearLayout) findViewById(R.id.One);
-        one.setOnClickListener(this);
-        two= (LinearLayout) findViewById(R.id.Two);
-        two.setOnClickListener(this);
-        three= (LinearLayout) findViewById(R.id.three);
-        three.setOnClickListener(this);
-        four= (LinearLayout) findViewById(R.id.four);
-        four.setOnClickListener(this);
-        viewPager= (ViewPager) findViewById(R.id.vp_tabs);
+        viewPager = (ViewPager) findViewById(R.id.vp_tabs);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.One:
-                Toast.makeText(this,"one",Toast.LENGTH_SHORT);
-                break;
-            case R.id.Two:
-                Toast.makeText(this,"Two",Toast.LENGTH_SHORT);
-                break;
-            case R.id.three:
-                Toast.makeText(this,"three",Toast.LENGTH_SHORT);
-                break;
-            case R.id.four:
-                Toast.makeText(this,"four",Toast.LENGTH_SHORT);
-                break;
-        }
-    }
 }
